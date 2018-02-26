1277f7b5e7fc58d9814891a2d0ff10333c3ccb92
lanket <info@lanket.ru>
2018-02-26 23:37:34 +0300

    Обновление от  fandaymon
    Новая версия файла - теперь отличает множественное число от единственного и если просили купить бананы, то так и добавит - бананы.
    Так же отслеживает случай, когда слово глагол и существительное, как ни странно таким является чай.

--- modules/app_products/product_addToListFromVoice.inc.php
+++ modules/app_products/product_addToListFromVoice.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-//$command='череный молотый перец белого лука гель для душа';
+//$command='черный молотый перец белого лука гель для душа йогурт марки денон красные розы';
 require_once(ROOT . "lib/phpmorphy/common.php");
 $opts = array(
  'storage' => PHPMORPHY_STORAGE_MEM,
@@ -29,23 +29,43 @@ $lang = 'ru_RU';
                 $base_forms[$is] = $morphy->getBaseForm($Word);
                 $partsOfSpeech[$is] = $morphy->getPartOfSpeech($Word);
                 $f_word[$is] = $morphy->getGramInfo($Word);
+            if ( count($partsOfSpeech[$is])==2) {
+                 if ($partsOfSpeech[$is][0]=="С" and $partsOfSpeech[$is][1]=="П") {
+                  $partsOfSpeech[$is][0]="П";
+                 } 
+                elseif ($partsOfSpeech[$is][0]=="Г" and $partsOfSpeech[$is][1]=="С") {
+          $partsOfSpeech[$is][0]="С";
+                 $base_forms[$is][0]=$base_forms[$is][1];
+         $chislo=array_intersect($f_word[$is][1][0]['grammems'],['ЕД', 'МН']);
+         $chislo=reset($chislo);
+          $rod=array_intersect($f_word[$is][1][0]['grammems'],['МР', 'ЖР', 'СР']);
+          $rod=reset($rod);
+                 $f_word[$is][0][0]['grammems'][0]=$cislo;
+                 $f_word[$is][0][0]['grammems'][1]=$rod;
+                  
+                 }
+                }
                 $base_forms[$is][]=$words[$is];
             } 
         }
 
 
+say('Добавляю в список покупок',2);
+
 for ($is = 0; $is < $totals; $is++) {
     if ($base_forms[$is][0]=='ВОД') $base_forms[$is][0]='ВОДА';
     if ($partsOfSpeech[$is][0]=='С') {
-    
+    $chislo=array_intersect($f_word[$is][0][0]['grammems'],['ЕД', 'МН']);
+    $chislo=reset($chislo);
         if (($is+1)<$totals) {
             if ($partsOfSpeech[$is+1][0]=='С') {
-        if ($base_forms[$is+1][0]=='МАРКА') {
+        if ($base_forms[$is+1][0]=='МАРКА' or $base_forms[$is+1][0]=='ФИРМА') {
             $product=$base_forms[$is][0]. ' ' . $words[$is+1] . ' ' . $words[$is+2];
             $is=$is+2;
         }
         else {
-            $product=$base_forms[$is][0];
+            $product=$morphy->castFormByGramInfo($base_forms[$is][0],'С',[$chislo,'ИМ']);
+            $product=$product[0]['form'];
         }
             }
             elseif ($partsOfSpeech[$is+1][0]=='ПРЕДЛ') {
@@ -53,96 +73,124 @@ for ($is = 0; $is < $totals; $is++) {
                 $is=$is+2;
             }
              elseif ($partsOfSpeech[$is+1][0]=='П') {
-                $product=$base_forms[$is][0] ;
-
+        $rod=array_intersect($f_word[$is+1][0][0]['grammems'],['МР', 'ЖР', 'СР']);
+        $rod=reset($rod);
+        // Выбираем форму прилагательного правильного рода
+        if ($chislo=='ЕД') {
+            $adjective=$morphy->castFormByGramInfo($base_forms[$is+1][0],"П",[$rod,'ЕД','ИМ']);
+        }
+        else {
+            $adjective=$morphy->castFormByGramInfo($base_forms[$is+1][0],"П",['МН','ИМ']);
+        }
+        $adjective=$adjective[0]['form'];
+        $noun=$morphy->castFormByGramInfo($base_forms[$is][0],'С',[$chislo,'ИМ']);
+        $noun=$noun[0]['form'];
+        if (Get_Product_ID($adjective . " " . $noun)>0) {
+            $product=$adjective . " " . $noun;
+            $is=$is+1;
+        }
+        else {
+            $product=$noun ;
+        }
             }
 
         }
         else {
-            $product=$base_forms[$is][0];
+        
+        $noun=$morphy->castFormByGramInfo($base_forms[$is][0],'С',[$chislo,'ИМ']);
+        $noun=$noun[0]['form'];
+            $product=$noun;
         }
     }
     elseif ($partsOfSpeech[$is][0]=='П') {
+    $rod=array_intersect($f_word[$is][0][0]['grammems'],['МР', 'ЖР', 'СР']);
+    $rod=reset($rod);
+    $chislo=array_intersect($f_word[$is][0][0]['grammems'],['МН', 'ЕД']);
+    $chislo=reset($chislo);
+    
+    // Выбираем форму прилагательного правильного рода
+    if ($chislo=='ЕД') {
+        $adjective=$morphy->castFormByGramInfo($base_forms[$is][0],"П",[$rod,'ЕД','ИМ']);
+    }
+    else {
+        $adjective=$morphy->castFormByGramInfo($base_forms[$is][0],"П",['МН','ИМ']);
+    }
+    $adjective=$adjective[0]['form'];
         if (($is+1)<$totals) {
             if ($partsOfSpeech[$is+1][0]=='С') {
-
                 if (count($base_forms[$is+1])>2){
                  // выбираем форму согласованную по роду
-                 for ($k= 0; $k < count($f_word[$is][0][0]['grammems']); $k++) {
-                  $gram=$f_word[$is][0][0]['grammems'][$k];
-                  if ($gram=='МР' or $gram=='СР' or $gram=='ЖР') {
-                   break;
-                  }
-                 }
-                 $kkk=0;
                  for ($kk= 0; $kk < count($base_forms[$is+1])-1; $kk++) {
-
-                  for ($k= 0; $k < count($f_word[$is+1][$kk][0]['grammems']); $k++) {
-                    if ($gram==$f_word[$is+1][$kk][0]['grammems'][$k]) {
-                     $kkk=1;
-                     break;
-                    }
-                  }
-                  if ($kkk==1) break;
+                       $rod1=array_intersect($f_word[$is+1][$kk][0]['grammems'],['МР', 'ЖР', 'СР']);
+                    $rod1=reset($rod1);
+                    if ($rod==$rod1) break;
                  }
-                $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][$kk] ;
-                $is=$is+1; 
-                 
+         $noun=$morphy->castFormByGramInfo($base_forms[$is+1][$kk],'С',[$chislo,'ИМ']);
+         $noun=$noun[0]['form'];
+
+                 $product=$adjective .' ' . $noun ;
+                 $is=$is+1; 
                 }
-                 else {
-                $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][0] ;
-                $is=$is+1; 
+                else {
+         $noun=$morphy->castFormByGramInfo($base_forms[$is+1][0],'С',[$chislo,'ИМ']);
+         $noun=$noun[0]['form'];
+
+                 $product=$adjective .' ' . $noun ;
+                 $is=$is+1; 
                 } 
             }
             elseif ($partsOfSpeech[$is+1][0]=='П') {
+                if ($chislo=='ЕД') {
+                    $adjective1=$morphy->castFormByGramInfo($base_forms[$is+1][0],"П",[$rod,'ЕД','ИМ']);
+                }
+                else {
+                    $adjective1=$morphy->castFormByGramInfo($base_forms[$is+1][0],"П",['МН','ИМ']);
+                }
+
+                $adjective1=$adjective1[0]['form'];
                 if (($is+2)<$totals) {
                     if ($partsOfSpeech[$is+2][0]=='С') {
 
                         if (count($base_forms[$is+2])>2){
                             // выбираем форму согласованную по роду
-                            for ($k= 0; $k < count($f_word[$is][0][0]['grammems']); $k++) {
-                                $gram=$f_word[$is][0][0]['grammems'][$k];
-                                if ($gram=='МР' or $gram=='СР' or $gram=='ЖР') break;
-                   
-                            }
-                 
-                            $kkk=0;
                             for ($kk= 0; $kk < count($base_forms[$is+2])-1; $kk++) {
-
-                                for ($k= 0; $k < count($f_word[$is+2][$kk][0]['grammems']); $k++) {
-                                    if ($gram==$f_word[$is+2][$kk][0]['grammems'][$k]) {
-                                        $kkk=1;
-                                        break;
-                                    }
-                                }
-                                if ($kkk==1) break;
+                                $rod1=array_intersect($f_word[$is+2][$kk][0]['grammems'],['МР', 'ЖР', 'СР']);
+                                $rod1=reset($rod1);
                             }
-                            $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][0] . ' ' . $base_forms[$is+2][$kk] ;
+                 $noun=$morphy->castFormByGramInfo($base_forms[$is+2][$kk],'С',[$chislo,'ИМ']);
+                 $noun=$noun[0]['form'];
+ 
+                            $product=$adjective .' ' . $adjective1 . ' ' . $noun ;
                             $is=$is+2; 
                  
                         }
                         else {
-                            $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][0] .' ' . $base_forms[$is+2][0];
+                 $noun=$morphy->castFormByGramInfo($base_forms[$is+2][0],'С',[$chislo,'ИМ']);
+                 $noun=$noun[0]['form'];
+
+                            $product=$adjective .' ' . $adjective1 .' ' . $base_forms[$is+2][0];
                             $is=$is+2; 
                         }
                     }
                     else {
-                        $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][0];
+                        $product=$adjective .' ' . $adjective1;
                         $is=$is+1;
                     }    
                 }
                 else {
-                    $product=$base_forms[$is][0] .' ' . $base_forms[$is+1][0];
+                    $product=$adjective .' ' . $adjective1;
                     $is=$is+1;
                 }    
             }                
         }
         else {
-            $product=$base_forms[$is][0];
+            $product=$adjective;
         }    
     }        
 
     $product = strtolower($product);
+
+    say($product,2);
      
     if($debugEnabled) debmes('Products produkt:'. $product);
                 
@@ -172,7 +220,6 @@ for ($is = 0; $is < $totals; $is++) {
            $Record['TITLE'] = $product;
           $Record['CATEGORY_ID'] = $category_id;
           $Record['QTY'] = 1;
-
            $Record['ID']=SQLInsert('products', $Record);
         $id = $Record['ID'];
 
